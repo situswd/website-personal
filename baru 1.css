07bf2572a94c55e95a0b089a209bfc86439cf4d5
situswd <cssituswd@gmail.com>
2024-12-19 14:22:58 +0700

    Add files via upload

---
+++ baru 1.css
@@ -0,0 +1,159 @@
+
+
+<!DOCTYPE html>
+<html amp lang="id">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="theme-color" content="black">
+  <title>>SITUSWD:TURNOVER BULANAN BERHADIAH HINGGA TOTAL 2 MILYAR RUPIAH</title>
+  <meta name="description" content="SITUSWD mendefinisikan ulang permainan slot online. Nikmati game anti rungkad dengan layanan pelanggan yang tak tertandingi.">
+  <link rel="canonical" href="###">
+  <meta property="og:type" content="WebPage">
+  <meta property="og:title" content="SITUSWD | Situs game Rekomendasi dan Terpercaya 2024">
+  <meta property="og:description" content="SITUSWD mendefinisikan ulang permainan slot online. Nikmati game anti rungkad dengan layanan pelanggan yang tak tertandingi.">
+  <meta property="og:site_name" content="SITUSWD">
+  <meta property="og:image" content="https://situs-slot-gacor-SITUSWD.myshopify.com/cdn/shop/files/slotgacor.jpg?v=1708457810">
+  <meta property="og:image:width" content="500">
+  <meta property="og:image:height" content="500">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="SITUSWD | Situs game Rekomendasi dan Terpercaya 2024">
+  <meta name="twitter:description" content="SITUSWD mendefinisikan ulang permainan slot online. Nikmati game anti rungkad dengan layanan pelanggan yang tak tertandingi.">
+  <meta name="twitter:image" content="https://i.ibb.co/42FscRM/image.png">
+  <link href="###" rel="dns-prefetch">
+  <link rel="icon" type="image/png" href="https://files.sitestatic.net/ImageFile/6277b6e16b97a_LGSKY.png">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap">
+  <link rel="preload" as="image" href="https://i.postimg.cc/h4NGqDTR/SN.png">
+  <link rel="preload" as="image" href="https://i.ibb.co/42FscRM/image.png">
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+  <style amp-boilerplate> body {-webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;-moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;-ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;animation: -amp-start 8s steps(1, end) 0s 1 normal both }@-webkit-keyframes -amp-start {from {visibility: hidden }to {visibility: visible }}@-moz-keyframes -amp-start {from {visibility: hidden }to {visibility: visible }}@-ms-keyframes -amp-start {from {visibility: hidden }to {visibility: visible }}@-o-keyframes -amp-start {from {visibility: hidden }to {visibility: visible }}@keyframes -amp-start {from {visibility: hidden }to {visibility: visible }}</style><noscript> <style amp-boilerplate> body {-webkit-animation: none;-moz-animation: none;-ms-animation: none;animation: none }</style> </noscript>
+  <style amp-custom>:root {--bg-color: #f44336;--subbg-color: #3673ed;--border-color: #ffffff;--marquee-color: #ffffff;--ta-bg: linear-gradient(0deg,#fac811,#fac811 48.74%,#fac811);--shadow: 4px 4px 4px #000000, -2px -2px 2px rgba(255, 255, 255, 0);--box-shadow: 0 13px 12px -10px rgb(0 0 0 / 29%), 0 0 6px 4px rgb(0 0 0 / 28%), 1px 10px 6px 10px rgb(0 0 0 / 0%), 0 0 6px 4px rgb(0 0 0 / 28%);--gradient-bg: linear-gradient(270deg, #3937ce, #0004ff 48.74%, #0400ff);--footer-bg: linear-gradient(270deg, #1100ff, #1900ff 48.74%, #2600ff);--footer-border: rgb(255,255,255);}body {font-family: 'Rubik', sans-serif;background-color: var(--bg-color);color: #fff;line-height: 1;}.content a, footer a {color: #0400ff;text-decoration: none;transition: color .2s,background-color .2s;font-weight: bold;}.content a:active,a:focus {outline: 0 }.content a:hover {text-decoration: none }.footer_menu a {color: #ffffff;}h1,h2,h3,h4,h5 {color: #ffffff;font-weight: 700 }h1 {font-size: 32px;line-height: 36px }h2 {font-size: 20px;line-height: 26px;padding-top: 10px }h3 {font-size: 18px;line-height: 24px }h4 {font-size: 16px;line-height: 22px }h5 {font-size: 15px;line-height: 26px }li {margin-top: 10px; line-height: 24px}strong {line-height: 24px}p {color: #f5f5f5;line-height: 26px }.mb10 {margin-bottom: 10px;}.headerbar {position: fixed;z-index: 999;top: 0;height: 50px;width: 100%;padding-top: 15px;margin-top: 0px;background: var(--gradient-bg);box-shadow: var(--box-shadow);text-align: center;}.marquee-text {height: 30px;display: block;line-height: 30px;overflow: hidden;position: relative;background: var(--gradient-bg);}.marquee-text:before, .marquee-text:after {content: '';position: absolute;width: 5px;height: 100%;background-color: var(--subbg-color);top: 0;z-index: 2;}.marquee-text:before {left: 0;}.marquee-text:after {right: 0;}.marquee-text div {display: block;width: 200%;height: 30px;line-height: 30px;position: absolute;overflow: hidden;font-size: 16px;color: var(--marquee-color);z-index: 1;animation: marquee 15s linear infinite;}.marquee-text span {float: left;width: 50%;}.marquee-text:hover div {animation-play-state: paused }@keyframes marquee {0% {left: 0;}100% {left: -100%;}}.header-button {display: flex;justify-content: space-between;padding: 10px;}.banner {margin-top: 67px;}.animated-button1 {background: linear-gradient(to bottom, #1900fd 0, #1100ff 100%);padding: 12px;width: calc(50% - 30px);display: block;-webkit-transform: translate(0, 0);transform: translate(0, 0);overflow: hidden;color: rgb(255, 255, 255);font-size: 20px;text-align: center;text-transform: uppercase;text-decoration: none;-webkit-box-shadow: inset 0 2px 0 rgb(255, 255, 255), 0 2px 2px rgb(255, 255, 255), 0 2px 4px 1px rgb(255, 255, 255);box-shadow: inset 0 2px 0 rgba(0, 0, 0, 0.5), 0 2px 2px rgba(0, 0, 0, .5), 0 2px 4px 1px rgba(0, 0, 0, .5);border-radius: 8px;}.animated-button1.signup {background: linear-gradient(to bottom, #0400ff 0, #0400ff 100%);color: rgb(255, 255, 255);}.animated-button1::before {content: '';position: absolute;top: 0;left: 0;width: 100%;height: 100%;background-color: #5e564c;opacity: 0;-webkit-transition: .2s opacity ease-in-out;transition: .2s opacity ease-in-out }.animated-button1:hover::before {opacity: .2 }.animated-button1 span {position: absolute }.animated-button1 span:nth-child(1) {top: 0;top: 0;left: 0;width: 100%;height: 60px;background: -webkit-gradient(linear, right top, left top, from(rgba(43, 8, 8, 0)), to(#0f0));background: linear-gradient(to left, rgba(43, 8, 8, 0), #fff);-webkit-animation: 2s animateTop linear infinite;animation: 2s animateTop linear infinite }@keyframes animateTop {0% {-webkit-transform: translateX(100%);transform: translateX(100%) }100% {-webkit-transform: translateX(-100%);transform: translateX(-100%) }}.animated-button1 span:nth-child(2) {top: 0;right: 0;height: 100%;width: 3px;background: -webkit-gradient(linear, left bottom, left top, from(rgba(43, 8, 8, 0)), to(#0f0));background: linear-gradient(to top, rgb(255, 255, 255), #fff);-webkit-animation: 2s animateRight linear -1s infinite;animation: 2s animateRight linear -1s infinite }@keyframes animateRight {0% {-webkit-transform: translateY(100%);transform: translateY(100%) }100% {-webkit-transform: translateY(-100%);transform: translateY(-100%) }}.animated-button1 span:nth-child(3) {bottom: 0;left: 0;width: 100%;height: 3px;background: -webkit-gradient(linear, left top, right top, from(rgba(43, 8, 8, 0)), to(#0f0));background: linear-gradient(to right, rgb(255, 255, 255), #fff);-webkit-animation: 2s animateBottom linear infinite;animation: 2s animateBottom linear infinite }@keyframes animateBottom {0% {-webkit-transform: translateX(-100%);transform: translateX(-100%) }100% {-webkit-transform: translateX(100%);transform: translateX(100%) }}.animated-button1 span:nth-child(4) {top: 0;left: 0;height: 100%;width: 3px;background: -webkit-gradient(linear, left top, left bottom, from(rgba(43, 8, 8, 0)), to(#0f0));background: linear-gradient(to bottom, rgba(43, 8, 8, 0), #fff);-webkit-animation: 2s animateLeft linear -1s infinite;animation: 2s animateLeft linear -1s infinite }@keyframes animateLeft {0% {-webkit-transform: translateY(-100%);transform: translateY(-100%) }100% {-webkit-transform: translateY(100%);transform: translateY(100%) }}.content {background-color: var(--subbg-color);padding: 30px 20px 20px 20px;border-radius: 8px;box-shadow: var(--shadow);margin: 10px;}.content h1{padding: 0;padding-bottom: 10px;margin: 0;font-size: 25px}ul{padding-left: 30px}.content_body {color: #c2c2c2;}.content_body blockquote {background-color: rgba(255, 255, 255, 0.1);padding: 10px;margin: 10px;border-radius: 8px;}.content_body blockquote p {margin: 0;}.content_body blockquote p:before, .content_body blockquote p:after {font-size: 24px;line-height: 0;opacity: .3;}.content_body blockquote p:before {content: '\275B\275B';margin-right: 5px;}.content_body blockquote p:after {content: '\275C\275C';margin-left: 5px;}footer {background: var(--footer-bg);border-top: 10px solid var(--footer-border);padding: 20px 10px;padding-bottom: 100px;text-align: center;border-radius: 8px 8px 0 0;border-top: 5px solid var(--footer-border) }.footdesc {font-size: 14px;opacity: .5;border-bottom: 1px solid var(--border-color);border-top: 1px solid var(--border-color);margin: 10px 0;}.poweredlog {margin-bottom: 10px;}.copyright {font-size: 14px;background: rgba(0,0,0,0.6);padding: 6px;border-radius: 8px;margin-top: 5px;margin-bottom: -20px;}.footer_menu {background: var(--footer-bg);box-shadow: var(--box-shadow);display: grid;grid-template-columns: repeat(5, 1fr);gap: 10px;text-align: center;padding: 10px 0;position: fixed;z-index: 999;bottom: 0;width: 100%;}.footer_menu a {display: block;font-size: 12px;text-decoration: none;}.footer_menu a span {display: block;margin-top: 5px;}.ftmenutop {position: relative;z-index: 10;}@media (min-width:1200px) {.container, footer {max-width: 1000px;margin: 0 auto; }}@media screen and (max-width: 600px) {.game {grid-template-columns: repeat(3, 1fr);}}</style>
+</head>
+<body>
+  
+  <div class="headerbar">
+    <a href="https://206.189.42.147">
+        <amp-img src="https://i.postimg.cc/h4NGqDTR/SN.png" width="125" height="50" alt="SITUSWDixed"></amp-img>
+      </a>
+  </div>
+  <div class="container">
+    <div class="banner">
+        <amp-img src="https://i.ibb.co/ZTf663q/TURNOVER.jpg" width="1540" height="880" alt="Slot Gacor" layout="responsive"></amp-img>
+    </div>
+    <div class="elementor-element elementor-element-527d2e2 e-con-full e-flex e-con e-parent e-lazyloaded" data-id="527d2e2" data-element_type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+				<div class="elementor-element elementor-element-38b744e elementor-widget__width-inherit elementor-widget-mobile__width-inherit e-transform elementor-widget elementor-widget-html" data-id="38b744e" data-element_type="widget" data-settings="{&quot;_transform_translateX_effect_mobile&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:-12,&quot;sizes&quot;:[]},&quot;_transform_translateX_effect&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:&quot;&quot;,&quot;sizes&quot;:[]},&quot;_transform_translateX_effect_tablet&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:&quot;&quot;,&quot;sizes&quot;:[]},&quot;_transform_translateY_effect&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:&quot;&quot;,&quot;sizes&quot;:[]},&quot;_transform_translateY_effect_tablet&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:&quot;&quot;,&quot;sizes&quot;:[]},&quot;_transform_translateY_effect_mobile&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:&quot;&quot;,&quot;sizes&quot;:[]}}" data-widget_type="html.default">
+				<div class="elementor-widget-container">
+			
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+  		</div>
+				</div>
+				</div>
+  </div>
+  <div class="container">
+    <div class="mb10">
+      <div class="marquee-text">
+        <div>
+          <span>IKUTI EVENT TURNOVER BULANAN DENGAN TOTAL HADIAH MENCAPAI 2 MILYAR RUPIAH</span>
+          <span>IKUTI EVENT TURNOVER BULANAN DENGAN TOTAL HADIAH MENCAPAI 2 MILYAR RUPIAH</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="header-button">
+      <a class="animated-button1" target="_blank" rel="nofollow noopener" href="t.ly/situswdpromo"><span></span><span></span><span></span><span></span>LOGIN</a>
+      <a class="animated-button1 signup" target="_blank" rel="nofollow noopener" href="https://206.189.42.147"><span></span><span></span><span></span><span></span>DAFTAR</a>
+    </div>
+    <div>
+       <style>
+        .table-container {
+            border-radius: 10px;
+            padding: 20px;
+            max-width: 1000px;
+            margin: auto;
+            color: white;
+        }
+        
+        .table-container table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        
+        .table-container th,
+        .table-container td {
+            padding: 10px;
+            border: 1px solid #333;
+            text-align: center;
+            color: white;
+        }
+        
+        .table-container th {
+            background-color: #f44336;
+            font-weight: bold;
+        }
+        
+        .table-container td {
+            background-color: #2799f4;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .table-container {
+                overflow-x: auto;
+            }
+
+            .table-container table {
+                width: 100%;
+                display: block;
+                overflow-x: auto;
+                white-space: nowrap;
+            }
+
+            .table-container th, .table-container td {
+                font-size: 11px;
+            }
+        }
+    </style>
+    </div>
+    <div>
+      <div class="table-container">
+        <div class="table-responsive">
+            <table id="data-table" class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>JUARA</th>
+                        <th>HADIAH</th>
+                        <th>USER ID</th>
+                        <th>TURNOVER</th>
+                    </tr>
+                </thead>
+                <tbody id="table-body">
+                    <!-- Data from Google Sheets will be inserted here -->
+                <tr><td>1</td><td>MOBIL New Agya GR Sport</td><td>MAR*****1</td><td>27,942,504,525</td></tr><tr><td>2</td><td>MOTOR Honda PCX160 ABS</td><td>MAD****2</td><td>26,141,228,200</td></tr><tr><td>3</td><td>MOTOR Yamaha Fazzio 2024</td><td>BAR*******1</td><td>18,128,254,700</td></tr><tr><td>4</td><td>SAMSUNG S-24 ULTRA</td><td>KAR*****3</td><td>17,025,232,337</td></tr><tr><td>5</td><td>IPHONE 15 PRO MAX 256 GB</td><td>LOV***1</td><td>15,253,321,950</td></tr><tr><td>6</td><td>IPAD PRO GENERASI KE-6</td><td>BLA********1</td><td>15,173,250,000</td></tr><tr><td>7</td><td>EMAS ANTHAM 10GRM</td><td>BOL****2</td><td>12,029,144,900</td></tr><tr><td>8</td><td>PLAYSTATION 5</td><td>UCU***0</td><td>9,873,155,100</td></tr><tr><td>9</td><td>NINTENDO SWITCH OLED</td><td>JPT******8</td><td>10,908,300,600</td></tr><tr><td>10</td><td>UANG TUNAI 2JT</td><td>ARO***5</td><td>7,942,504,525</td></tr><tr><td>11</td><td>UANG TUNAI 2JT</td><td>ANG******1</td><td>6,847,552,252</td></tr><tr><td>12</td><td>UANG TUNAI 2JT</td><td>UYE***1</td><td>6,128,254,700</td></tr><tr><td>13</td><td>UANG TUNAI 2JT</td><td>KUP***1</td><td>5,825,232,337</td></tr><tr><td>14</td><td>UANG TUNAI 2JT</td><td>JOK****1</td><td>5,518,518,745</td></tr><tr><td>15</td><td>UANG TUNAI 2JT</td><td>CUC****5</td><td>5,107,517,500</td></tr><tr><td>16</td><td>UANG TUNAI 1JT</td><td>PIK******5</td><td>4,641,412,800</td></tr><tr><td>17</td><td>UANG TUNAI 1JT</td><td>CIN****0</td><td>4,608,120,958</td></tr><tr><td>18</td><td>UANG TUNAI 1JT</td><td>ALK*****8</td><td>4,580,547,000</td></tr><tr><td>19</td><td>UANG TUNAI 1JT</td><td>FAH*****1</td><td>3,714,091,832</td></tr><tr><td>20</td><td>UANG TUNAI 1JT</td><td>SOP*****5</td><td>2,762,478,800</td></tr><tr><td>21</td><td>UANG TUNAI 500RB</td><td>MTU*****1</td><td>3,617,338,800</td></tr><tr><td>22</td><td>UANG TUNAI 500RB</td><td>HAD****1</td><td>2,395,489,000</td></tr><tr><td>23</td><td>UANG TUNAI 500RB</td><td>GDV****0</td><td>1,679,256,400</td></tr><tr><td>24</td><td>UANG TUNAI 500RB</td><td>PRO****T</td><td>1,435,950,903</td></tr><tr><td>25</td><td>UANG TUNAI 500RB</td><td>RAD*****8</td><td>973,105,450</td></tr><tr><td>26</td><td>UANG TUNAI 250RB</td><td>UNT****X</td><td>851,301,880</td></tr><tr><td>27</td><td>UANG TUNAI 250RB</td><td>SOJ*****3</td><td>810,092,935</td></tr><tr><td>28</td><td>UANG TUNAI 250RB</td><td>ART*****1</td><td>700,335,098</td></tr><tr><td>29</td><td>UANG TUNAI 250RB</td><td>FUD***7</td><td>693,167,851</td></tr><tr><td>30</td><td>UANG TUNAI 250RB</td><td>MAX********2</td><td>501,328,780</td></tr></tbody>
+            </table>
+        </div>
+   </div>
+   <div class="container">
+    <div class="content">
+      <h1>SITUSWD | Situs game Rekomendasi dan Terpercaya 2024</h1>
+    <p>SITUSWD mendefinisikan ulang permainan slot online. Nikmati game anti rungkad dengan layanan pelanggan yang tak tertandingi.</p>
+    </div>
+  </div>
+  
+  <footer>
+    <div class="footlog">
+      <amp-img src="https://i.postimg.cc/h4NGqDTR/SN.png" width="113" height="50" alt="onix gaming" layout="fixed"></amp-img>
+    </div>
+    <div class="copyright">© 2024 <a href="t.ly/situswdpromo">SITUSWD</a>. All rights reserved | 18+</div>
+  </footer>
+  </div>
+</body>
+</html>
